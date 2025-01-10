96a85975e94b1b8107ed2ff05f118ff3eff3ae0a
michael.momoh <122442992+Michealmomoh@users.noreply.github.com>
2025-01-10 22:27:32 +0000

    Delete script directory

--- script/getDates.js
+++
@@ -1,6 +0,0 @@
-const currentYear = new Date().getFullYear();
-document.getElementById('2025').textContent = currentYear;
-
-const lastModified = new Date(document.lastModified);
-document.getElementById('lastModified').textContent = 
-  `Last Modified: ${lastModified.toDateString()} at ${lastModified.toLocaleTimeString()}`;
