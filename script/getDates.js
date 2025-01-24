399b99b5225c5491ceb907be2ff585d6eca3aee4
michael momoh <michaelmomoh18@gmail.com>
2025-01-24 01:12:17 +0000

    SOME CHANGES

--- script/getDates.js
+++ script/getDates.js
@@ -4,3 +4,10 @@ document.getElementById('2025').textContent = currentYear;
 const lastModified = new Date(document.lastModified);
 document.getElementById('lastModified').textContent = 
   `Last Modified: ${lastModified.toDateString()} at ${lastModified.toLocaleTimeString()}`;
+
+
+  const visitCounterElement = document.getElementById('visitCounter');
+  let visits = Number(localStorage.getItem('visits')) || 0;
+  visits++;
+  visitCounterElement.textContent = visits;
+  localStorage.setItem('visits', visits);
